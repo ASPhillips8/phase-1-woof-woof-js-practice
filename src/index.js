dcb099f18185497a69b3dc243764302fc865b6d1
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-24 09:22:37 -0400

    Clean up comments and white space

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", () =>  {
 
   const url = "http://localhost:3000/pups"
+  const dogBar = document.querySelector("#dog-bar")
+  const dogInfo = document.querySelector("#dog-info")
 
   fetch(url)
   .then ((response) => response.json())
   .then((dogData) => dogData.forEach((dog) => renderDogBar(dog)))
-
-
-
-  const dogBar = document.querySelector("#dog-bar")
-  const dogInfo = document.querySelector("#dog-info")
 
   function renderDogBar(dog) {
 
@@ -21,79 +18,55 @@ document.addEventListener("DOMContentLoaded", () =>  {
     span.textContent = `${dog.name}`
     span.style.cursor = "pointer"
 
+    dogBar.appendChild(span)
+
     span.addEventListener("click" , () => {
       showDog(dog)
     })
 
-  function showDog(dog) {
+    function showDog(dog) {
 
-    dogInfo.textContent = ""
+      dogInfo.textContent = ""
 
-    const img = document.createElement("img")
-    const h2 = document.createElement("h2")
-    const goodButton = document.createElement("button")
+      const img = document.createElement("img")
+      const h2 = document.createElement("h2")
+      const goodButton = document.createElement("button")
 
-    img.src = `${dog.image}`
-    h2.textContent = `${dog.name}`
-    goodButton.textContent = "Good Dog!"
+      img.src = `${dog.image}`
+      h2.textContent = `${dog.name}`
+      goodButton.textContent = dog.isGoodDog ? "Good Dog!" : "Bad Dog!"
 
-    dogInfo.appendChild(img)
-    dogInfo.appendChild(h2)
-    dogInfo.appendChild(goodButton)
-
-    // clicks good/bad button
-    goodButton.addEventListener("click", () => {
-
-      let isGoodDog = goodButton.textContent === "Good Dog!"
-      let dogStatus = {
-        isGoodDog: !isGoodDog
-      }
-
-      fetch(`http://localhost:3000/pups/${dog.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify(dogStatus)
-    })
-    .then((response) => response.json())
-    .then(() => {
-      if (isGoodDog) {
-        goodButton.textContent = "Bad Dog!"
-      } else {
-        goodButton.textContent = "Good Dog!"
-      }
-    })
-    .catch((e) => alert(e.message))
-  });
+      dogInfo.appendChild(img)
+      dogInfo.appendChild(h2)
+      dogInfo.appendChild(goodButton)
 
 
+      goodButton.addEventListener("click", () => {
 
+        let isGoodDog = (goodButton.textContent === "Good Dog!")
+        let dogStatus = {
+          isGoodDog: !isGoodDog
+        }
 
-
-
-    // should be updated
-
-
-
+        fetch(`http://localhost:3000/pups/${dog.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify(dogStatus)
+      })
+      .then((response) => response.json())
+      .then(() => {
+        if (isGoodDog) {
+          goodButton.textContent = "Bad Dog!"
+        } else {
+          goodButton.textContent = "Good Dog!"
+        }
+      })
+      .catch((e) => alert(e.message))
+    });
+    }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-  dogBar.appendChild(span)
-  }
-
-
-
 })
 
