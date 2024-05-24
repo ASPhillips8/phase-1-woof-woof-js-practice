7f7d17514ef6d357e0629899b794e1df56590c55
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-24 00:20:01 -0400

    Crete event that display dog info

--- src/index.js
+++ src/index.js
@@ -13,18 +13,36 @@ document.addEventListener("DOMContentLoaded", () =>  {
 
   function renderDogBar(dog) {
 
-  const span = document.createElement("span")
-  const button = document.createElement("button")
-  const img = document.createElement("img")
-  const h2 = document.createElement("h2")
-  const goodButton = document.createElement("button")
+    const span = document.createElement("span")
+    const img = document.createElement("img")
+    const h2 = document.createElement("h2")
+    const goodButton = document.createElement("button")
 
-  span.textContent = `${dog.name}`
-  span.style.cursor = "pointer"
+    span.textContent = `${dog.name}`
+    span.style.cursor = "pointer"
 
-  span.addEventListener("click" , () => {
-    console.log("click")
-  })
+    span.addEventListener("click" , () => {
+      showDog(dog)
+    })
+
+  function showDog(dog) {
+
+    dogInfo.textContent = ""
+
+    const img = document.createElement("img")
+    const h2 = document.createElement("h2")
+    const goodButton = document.createElement("button")
+
+    img.src = `${dog.image}`
+    h2.textContent = `${dog.name}`
+    goodButton.textContent = "Good Dog!"
+
+    dogInfo.appendChild(img)
+    dogInfo.appendChild(h2)
+    dogInfo.appendChild(goodButton)
+
+
+  }
 
 
 
